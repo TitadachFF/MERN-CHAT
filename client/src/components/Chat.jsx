59cda553742c8ffa5578212a4f2a5c52462a77d0
TitadachFF <titadach15632441@gmail.com>
2024-01-18 17:09:29 +0700

    Make chat page

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import { useState, useEffect, useContext, useRef } from "react";
+import { UserContext } from "../context/UserContext";
+import axios from "axios";
+import Logo from "./Logo";
+import Contact from "./Contact";
 
 const Chat = () => {
   return (
-    <div>Chat</div>
-  )
-}
+    <div className="flex h-screen">
+      <div className="bg-white w-1/3 flex flex-col">
+        <div className="flex-grow font-semibold">
+          <Logo />
+          <Contact
+            username={"test12"}
+            id={"65a8bec6b657b2384ab7576a"}
+            online={true}
+            selected={true}
+          />
 
-export default Chat
+          <Contact
+            username={"test123"}
+            id={"65a8bf8f863135e913046003"}
+            online={false}
+            selected={false}
+          />
+        </div>
+        <div className="p-2 text-center flex item-center justify-center font-semibold ">
+          <span className="mr-2 text-sm text-gray-600 flex item-center mt-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+              />
+            </svg>
+            Username
+          </span>
+
+          <button className="text-sm bg-blue-100 py-1 px-2 text-gray-500 border rounded-sm hover:bg-blue-400 hover:text-white">
+            Logout
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-col bg-blue-50 w-2/3 p-2">
+        <div className="flex-grow">
+          <div className="flex h-full flex-grow items-center justify-center">
+            <div className="text-gray-300">
+              &larr;Select a person from sidebar
+            </div>
+          </div>
+        </div>
+        <from className="flex gap-2">
+          <input
+            type="text"
+            className="bg-white flex-grow border rounded-sm p-2"
+          />
+          <label
+            htmlFor=""
+            className="bg-blue-200 p-2 text-gray-600 cursor-pointer rounded-sm border border-blue-200 hover:bg-blue-300"
+          >
+            <input type="file" name="" id="" className="hidden" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13"
+              />
+            </svg>
+          </label>
+          <button
+            type="submit"
+            className="bg-blue-500 p-2 text-white rounded-sm hover:bg-blue-700"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+              />
+            </svg>
+          </button>
+        </from>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
--- client/src/components/RegisterAndLoginFrom.jsx
+++ client/src/components/RegisterAndLoginFrom.jsx
@@ -2,72 +2,83 @@ import React, { useContext, useState } from "react";
 import axios from "axios";
 import { UserContext } from "../context/UserContext";
 
-const RegisterAndLoginFrom = () => {
+const RegisterAndLoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isLoginOrRegister, setIsLoginOrRegister] = useState("login");
-  //Rename setUsername
+  // Rename setUsername
   const { setUsername: setLoggedInUsername, setId } = useContext(UserContext);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const url = isLoginOrRegister === "register" ? "register" : "login";
-    const { data } = await axios.post(url, { username, password });
-    setIsLoginOrRegister(username);
-    set(data.id);
+    try {
+      const { data } = await axios.post(url, { username, password });
+      setLoggedInUsername(username);
+      setId(data.id);
+    } catch (error) {
+      console.log(error);
+    }
   };
-  return (
 
-    <div className="bg-blue-50 h-screen flex item-center">
-      <form onSubmit={handleSubmit} className="w-64 mx-auto mb-12">
-        <input
-          type="text"
-          value={username}
-          className="block w-full rounded-sm pd-2 mb-2 border"
-          placeholder="Username"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="text"
-          value={password}
-          className="block w-full rounded-sm pd-2 mb-2 border"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="bg-blue-500 text-white block w-full rounded-sm p-2 hover:bg-blue-700">
-          
-          {isLoginOrRegister === "register" ? "Register" : "Login"}
-        </button>
-        <div className="text-center mt-2">
-          {isLoginOrRegister === "register" && (
-            <div>
-              Already a member ?{" "}
-              <button
-                className="ml-1 hover:text-blue-700 "
-                onClick={() => {
-                  setIsLoginOrRegister("login");
-                }}
-              >
-                Login here
-              </button>{" "}
-            </div>
-          )}
-          {isLoginOrRegister === "login" && (
-            <div>
-              Don't have an account{" "}
-              <button
-                className="ml-1 hover:text-blue-700"
-                onClick={() => {
-                  setIsLoginOrRegister("register");
-                }}
-              >
-                Register
-              </button>{" "}
-            </div>
-          )}
-        </div>
-      </form>
+  return (
+    <div className="bg-blue-50 h-screen flex items-center justify-center">
+      <div className="bg-white rounded-lg p-6 shadow-md w-[350px] mx-auto mb-12">
+        <h1 className="flex justify-center  text-[20px] font-bold text-blue-500 mb-5">
+          WELCOME TO MERN-CHAT
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={username}
+            className="block w-full rounded-lg p-2 mb-2 border font-semibold "
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="text"
+            value={password}
+            className="block w-full rounded-lg p-2 mb-2 border font-semibold"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="border-b-[1px] p-5 border-gray-300">
+            <button className="bg-blue-500 text-white block w-full rounded-lg p-2  hover:bg-blue-700 font-bold mt-5	text-decoration-line: underline; ">
+              {isLoginOrRegister === "register" ? "Register" : "Login"}
+            </button>
+          </div>
+          <div className="text-center mt-5  border-gray-300 text-gray-500   ">
+            {isLoginOrRegister === "register" && (
+              <div>
+                Already a member?{" "}
+                <button
+                  className="ml-1 text-blue-600 hover:text-blue-700 "
+                  onClick={() => {
+                    setIsLoginOrRegister("login");
+                  }}
+                >
+                  Login here
+                </button>{" "}
+              </div>
+            )}
+            {isLoginOrRegister === "login" && (
+              <div>
+                Don't have an account?{" "}
+                <button
+                  className="ml-1 text-red-500 hover:text-blue-700 "
+                  onClick={() => {
+                    setIsLoginOrRegister("register");
+                  }}
+                >
+                  Register
+                </button>{" "}
+              </div>
+            )}
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default RegisterAndLoginFrom;
+export default RegisterAndLoginForm;
